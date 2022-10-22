f4d4ca6cd2c54bdba62f92c5bdae67e34d909d7f
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2022-10-23 06:29:13 +0700

    create userModel

---
+++ backend/models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add a name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please add a email"],
+        unique: true,
+        trim: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please enter a valid email"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        minlength: [8, "Password must be up to 8 characters"],
+        maxlength: [25, "Password must not be more than 25 characters"]
+    },
+    photo: {
+        type: String,
+        required: [true, "Please add a photo"],
+        default: "https://i.ibb.co/4pDNDk1/avatar.png"
+    },
+    phone: {
+        type: String,
+        default: "+62"
+    },
+    bio: {
+        type: String,
+        maxL: [250, "Bio must not be more than 250 characters"],
+        default: "bio"
+    }
+}, {
+    timestamps: true
+})
+
+const User = mongoose.model("User", userSchema)
+module.exports = User
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
@@ -8,12 +8,22 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// routes
+app.get('/', (req, res) => {
+    res.send("Home Page")
+})
+
 // connect to DB
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(PORT, () => {
-            console.log(`Server Running on port ${PORT}`);
+            console.log(`Server Running on port ${PORT}`)
         })
     })
     .catch((err) => console.log(err))
